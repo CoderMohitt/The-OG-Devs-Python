a73dcce3d9675d4ae291c6c6c1823f6ce926ec2a
CoderMohitt <mohitjoshiyt@gmail.com>
2025-03-29 21:06:57 +0530

    Add files via upload

---
+++ Day 02/new.py
@@ -0,0 +1,61 @@
+# print('hello world')
+# print('print is a keyword')
+
+a = 80 #integer
+b = 89.98 #floating point numbers
+c = 'mohit' #string
+d = True #boolean
+e = None #none
+
+a1 = 50
+a_1 = 50
+A_1 = 50
+_b = 20
+
+# 1a = 90
+# my name = 'mohit'
+
+#Arithmetic Operators -> + - * /
+
+a = 20
+
+a = 'rajat'
+# print(a)
+
+a = 200
+b = 50
+
+c = a + b
+c = a - b
+c = a * b
+c = a / b
+
+# print(c)
+
+a = 2
+# a = a + 1
+a += 1
+# print(a)
+
+# a = a - 1
+a -= 1
+# print(a)
+
+a = 100
+b = 10
+
+# print(a == b)
+# print(a > b)
+# print(a >= b)
+# print(a < b)
+# print(a <= b)
+# print(a != b)
+
+#logical operators -> and or not
+
+# print(10 == 10)
+# print(20 > 10)
+
+print(10 == 100 and 20 > 10)
+
+print(10 == 100 or 20 > 10)
