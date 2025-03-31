270539936b5487711496a72ad92e2dc2404e67e9
CoderMohitt <mohitjoshiyt@gmail.com>
2025-03-31 20:50:33 +0530

    Add files via upload

---
+++ Day 03/new.py
@@ -0,0 +1,24 @@
+a = 10
+# print(type(a))
+
+a = 10.45
+# print(type(a))
+
+a = 'mohit'
+# print(type(a))
+
+a = True
+# print(type(a))
+
+a = None
+# print(type(a))
+
+a = 31
+b = str(a)
+b = float(a)
+# print(type(b))
+
+# print(type(int('32')))
+
+c = int(input('Enter Your Name : '))
+print(type(c))
---
+++ Day 03/prac.py
@@ -0,0 +1,32 @@
+a = 10
+b = 20
+
+c = a + b
+# print(a + b)
+
+# modulus operator
+
+a = 23 % 2 
+# print(a)
+
+
+# print(type(a))
+# print(type(b))
+
+# print(a % b)
+
+# print('this is after error')
+
+a = 34
+b = 80
+
+#== < > <= >= !=
+
+# print(a < b)
+# print(a > b)
+
+a = int(input('Enter First Number : '))
+# b = int(input('Enter Second Number : '))
+
+# print((a + b) / 2)
+print(a * a)
