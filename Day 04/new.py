a16f78b925b7cf68bb3cad6e7cf7bdc1bf6a51aa
CoderMohitt <mohitjoshiyt@gmail.com>
2025-04-01 21:02:34 +0530

    Add files via upload

---
+++ Day 04/new.py
@@ -0,0 +1,68 @@
+print('hello world')
+
+# a = 'name'
+# print(type(a))
+
+a = 'single quoted string'
+b = "double quoted string"
+c = '''triple quoted string'''
+
+# print(type(a))
+# print(type(b))
+# print(type(c)
+
+a = 'laptop'
+
+# print(a[0])
+# print(a[1])
+# print(a[3])
+# print(a[5])
+# print(a[6])
+
+# print(len(a))
+
+# print(a[-1]) #6 - 1  = 5
+# print(a[5])
+# print(a[-2]) # 6 - 2 = 4
+# print(a[4])
+
+
+a = 'banana'
+
+sl = a[1 : 5]
+
+# print(sl)
+
+# b     a   n   a   n   a
+
+# c  o  m    p   u   t   e   r
+# 0  1  2    3   4   5   6   7
+
+b = 'computer'
+sl = b[0 : 6]
+sl1 = b[0 : 6 : 2]
+
+# print(sl)
+# print(sl1)
+
+c = 't_h_i_s_i_s_a_c_o_m_p_u_t_e_r'
+# print(len(c))
+# print(c[28])
+sl = c[0:28:2]
+# print(sl)
+
+# print(c[0:])
+# print(c[:28])
+
+d = 'mohit'
+# print(len(d))
+
+# print(d.endswith('e'))
+# print(d.count('o'))
+# print(d.capitalize())
+# print(d.find('t'))
+
+e = 'this is \t this a string'
+# print(e.replace('this', 'that'))
+
+print(e)
