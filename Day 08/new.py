4e3a97607b09c44d2f39ac08ce2397e6dc316d1f
CoderMohitt <mohitjoshiyt@gmail.com>
2025-04-05 21:06:16 +0530

    Add files via upload

---
+++ Day 08/new.py
@@ -0,0 +1,100 @@
+# age = 12
+
+# if(age > 18):
+#     print('yes you can vote')
+# else:
+#     print('no you cant')
+
+# a = 10
+
+# if(a > 10):
+#     print('great')
+# else:
+#     print('no')
+
+# age = int(input('Enter your age : '))
+
+# if(age >= 18):
+#     print('yes')
+
+age = 90
+
+#age >= 18 you can drive
+#age < 18 you cant drive
+#age < 10 soch bhi mat
+
+# if(age >= 18 and age < 80):
+#     print('you can drive')
+# elif(age < 10):
+#     print('soch bhi mat')
+# elif(age >= 80):
+#     print('nice try!! better luck next janam')
+# else:
+#     print('you cant drive')
+
+# a = int(input('Enter first number : '))
+# b = int(input('Enter second number : '))
+# c = int(input('Enter third number : '))
+# d = int(input('Enter fourth number : '))
+
+# if(a > b and a > c and a > d):
+#     print('a is greatest')
+# elif(b > a and b > c and b > d):
+#     print('b is greatest')
+# elif(c > a and c > b and c > d):
+#     print('c is greatest')
+# else:
+#     print('d is greatest')
+
+# maths = 33
+# phy = 50
+# che = 50
+
+# total = ((maths + phy + che) * 100)  / 300
+
+# if(total > 40):
+#     if(maths >= 33 and phy >= 33 and che >= 33):
+#         print('pass')
+# else:
+#     print('fail')
+
+
+# spam = ['make a lot of money', 'subscribe this', 'click this']
+
+# user_comment = 'click this'
+
+# if(spam[0] == user_comment or spam[1] == user_comment or spam[2] == user_comment):
+#     print('spam detected')
+# else:
+#     print('normal comment')
+
+# username = 'mohit.jo'
+
+# if(len(username) >= 10):
+#     print('valid username')
+# else:
+#     print('username should have at least 10 chars')
+
+# lst = ['rohit', 'rahul', 'rajat', 'mansi', 'paras']
+
+# name = 'rahul'
+
+# if(name == lst[0] or name == lst[1] or name == lst[2] or name == lst[3] or name == lst[4]):
+#     print('yes its in the list')
+# else:
+#     print('no its not there')
+
+total = 80
+
+if(total > 89 and total < 101):
+    print('excellent')
+elif(total > 79 and total < 91):
+    print('A')
+elif(total > 69 and total < 81):
+    print('B')
+elif(total > 59 and total < 71):
+    print('C')
+elif(total > 49 and total < 61):
+    print('D')
+else:
+    print('F')
