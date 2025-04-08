b3c13d983e512ec801e9ce2f91b720cf01ee4510
CoderMohitt <mohitjoshiyt@gmail.com>
2025-04-08 20:57:16 +0530

    Add files via upload

---
+++ Day 09/new.py
@@ -0,0 +1,52 @@
+# i = 1
+
+# while i < 5:
+#     print('nit op')
+#     i = i + 1
+
+
+
+# print(lst[0])
+# print(lst[1])
+# print(lst[2])
+# print(lst[3])
+# print(lst[4])
+
+# i = 0
+
+# while i < 5:
+#     print(lst[i])
+#     i = i + 1
+
+# for i in lst:
+#     print(i)
+
+# for i in range(1, 11):
+#     print(i)
+
+# for a in range(10, 51):
+#     print(a)
+
+lst = ['rajat', 'rahul', 'rohit', 'meenakshi', 'lalit']
+
+# for item in lst:
+#     print(item)
+# else:
+#     print('done')
+
+i = 1
+
+# while i < 11:
+#     if(i == 5):
+#         continue
+#     print(i)
+#     i = i + 1
+
+# for i in range(1, 11):
+#     if(i == 5):
+#         continue
+#     print(i)
+
+for i in range(1, 11):
+    #rohit says we can use comment
+    pass
